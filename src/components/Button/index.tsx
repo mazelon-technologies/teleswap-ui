0b39fd99baa2c32a73bc4cfd036865fca88d9489
SunuSundar <junesunu@gmail.comm>
2021-09-22 11:42:18 +0530

    New Theme updated

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -83,7 +83,7 @@ export const ButtonPrimary = styled(Base)`
 
 export const ButtonLight = styled(Base)`
   background-color: ${({ theme }) => theme.primary5};
-  color: ${({ theme }) => theme.primaryText1};
+  color: ${({ theme }) => theme.text1};
   font-size: 16px;
   font-weight: 500;
   &:focus {
--- src/components/PositionCard/index.tsx
+++ src/components/PositionCard/index.tsx
@@ -42,7 +42,7 @@ export const HoverCard = styled(Card)`
 const StyledPositionCard = styled(LightCard)<{ bgColor: any }>`
   border: none;
   background: ${({ theme, bgColor }) =>
-    `radial-gradient(91.85% 100% at 1.84% 0%, ${transparentize(0.8, bgColor)} 0%, ${theme.bg3} 100%) `};
+    `radial-gradient(91.85% 100% at 1.84% 0%, ${transparentize(0.8, '#bf4ed0')} 0%, ${theme.bg2} 100%) `};
   position: relative;
   overflow: hidden;
 `
